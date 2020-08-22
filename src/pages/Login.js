722967f8a579fd9e595dd808b0ce1c7f0bca2862
Camila Tigre <milatigre1@gmail.com>
2020-08-21 23:51:11 -0300

    feat(validation): adicionando validação de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,49 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "../styles/pages/Login.css";
 import Header from "../common/Header";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import { fieldsValidation } from "../utils/validation";
 
 const Login = () => {
+  const [email, setEmail] = useState({ name: "email", value: "", helper: "" });
+  const [password, setPassword] = useState({
+    name: "password",
+    value: "",
+    helper: "",
+  });
+
+  const onChange = (evt) => {
+    const { name, value } = evt.target;
+
+    switch (name) {
+      case "email":
+        setEmail({ name, value, helper: "" });
+        break;
+      case "password":
+        setPassword({ name, value, helper: "" });
+        break;
+    }
+  };
+
+  const enterOnApp = () => {
+    const notValidFields = fieldsValidation([email, password]);
+
+    console.log(notValidFields);
+
+    if (notValidFields.length > 0) {
+      notValidFields.forEach((field) => {
+        if (field.name === "email") {
+          setEmail(field);
+        }
+
+        if (field.name === "password") {
+          setPassword(field);
+        }
+      });
+    }
+  };
+
   return (
     <Fragment>
       <Header whiteLogo={true} />
@@ -18,22 +57,34 @@ const Login = () => {
             fullWidth
             label="Digite seu E-MAIL"
             type="email"
+            name="email"
             required
             variant="outlined"
+            onChange={onChange}
+            helperText={email["helper"]}
+            value={email["value"]}
           />
           <TextField
             className="textField"
             fullWidth
             label="Digite sua SENHA"
             type="password"
+            name="password"
             required
             variant="outlined"
+            onChange={onChange}
+            helperText={password["helper"]}
+            value={password["value"]}
           />
           <div className="buttonContainer">
             <Button variant="outlined" className="secondaryButton">
               Quero me cadastrar
             </Button>
-            <Button variant="contained" className="primaryButton">
+            <Button
+              onClick={enterOnApp}
+              variant="contained"
+              className="primaryButton"
+            >
               Entrar
             </Button>
           </div>
---
+++ src/utils/validation.js
@@ -0,0 +1,54 @@
+export const fieldsValidation = (fields) => {
+  let validFields = [];
+
+  for (let field in fields) {
+    let currentField = fields[field];
+    let helper = fieldsRulesByName(currentField);
+
+    currentField = {
+      name: currentField.name,
+      value: currentField.value,
+      helper,
+    };
+
+    if (currentField.helper !== "") {
+      validFields.push(currentField);
+    }
+  }
+
+  console.log(validFields);
+
+  return validFields;
+};
+
+const fieldsRulesByName = (field) => {
+  if (field.name === "email") {
+    return emailRulesToValidation(field.value);
+  }
+
+  if (field.name === "password") {
+    return passwordRulesToValidation(field.value);
+  }
+};
+
+const emailRulesToValidation = (value) => {
+  if (value === "") {
+    return "Favor digitar um e-mail.";
+  }
+
+  let regex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+
+  if (!regex.test(value)) {
+    return "Favor digitar um e-mail válido.";
+  }
+
+  return "";
+};
+
+const passwordRulesToValidation = (value) => {
+  if (value === "") {
+    return "Favor digitar uma senha.";
+  }
+
+  return "";
+};
